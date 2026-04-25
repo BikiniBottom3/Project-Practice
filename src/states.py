88f2ed65b91559d5fa06a7fae05ee49dec3c0985
0squidy0 <136806144+0squidy0@users.noreply.github.com>
2026-04-25 18:48:17 +0300

    Update states.py

--- src/states.py
+++ src/states.py
@@ -1,1 +1,6 @@
+from aiogram.fsm.state import State, StatesGroup
 
+class AddHabitStates(StatesGroup):
+    waiting_for_name = State()
+    waiting_for_description = State()
+    waiting_for_reminder_time = State()
