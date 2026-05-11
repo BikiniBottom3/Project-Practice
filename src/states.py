3323da8e844f365b09c5b1bd847e277df90a1d7e
0squidy0 <136806144+0squidy0@users.noreply.github.com>
2026-05-11 16:40:41 +0300

    Delete src/states.py

--- src/states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AddHabitStates(StatesGroup):
-    waiting_for_name = State()
-    waiting_for_description = State()
-    waiting_for_reminder_time = State()
